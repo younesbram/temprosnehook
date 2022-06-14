d9480b42ceb0469a6aedbc806d44fad318652d47
Bintr <20655963+explowz@users.noreply.github.com>
2022-06-14 23:05:42 +0300

    Add check for cond

--- src/hacks/AntiTaunts.cpp
+++ src/hacks/AntiTaunts.cpp
@@ -18,7 +18,7 @@ void cm()
     for (unsigned int i = 1; i <= g_IEngine->GetMaxClients(); i++)
     {
         ent = ENTITY(i);
-        if (CE_BAD(ent) || ent == LOCAL_E || ent->m_Type() != ENTITY_PLAYER)
+        if (!HasCondition<TFCond_Taunting>(ent) && (CE_BAD(ent) || ent->m_Type() != ENTITY_PLAYER))
             continue;
 
         RemoveCondition<TFCond_Taunting>(ent);

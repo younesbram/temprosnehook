aba8fd687f964ff09a8078577e416ae8e80a23d2
rosne <68449858+rosne-gamingyt@users.noreply.github.com>
2023-06-15 19:47:22 +0000

    Update MiscAimbot.hpp

--- include/hacks/MiscAimbot.hpp
+++ include/hacks/MiscAimbot.hpp
@@ -2,8 +2,8 @@
 #include "common.hpp"
 namespace hacks::misc_aimbot
 {
-std::pair<CachedEntity , Vector> FindBestEnt(bool teammate, bool Predict, bool zcheck, bool fov_check, float range = 1500.0f);
+std::pair<CachedEntity *, Vector> FindBestEnt(bool teammate, bool Predict, bool zcheck, bool fov_check, float range = 1500.0f);
 void DoSlowAim(Vector &input_angle, int speed = 4);
-bool ShouldHitBuilding(CachedEntityent);
+bool ShouldHitBuilding(CachedEntity *ent);
 bool ShouldChargeAim();
 } // namespace hacks::misc_aimbot

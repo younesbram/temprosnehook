dec9307ff6391cf2c8867da08bce8fb96a67bb3e
rosne <68449858+rosne-gamingyt@users.noreply.github.com>
2023-06-25 10:52:14 -0400

    Revert "pls fix deathstare"
    This reverts commit 4f82b1fa8c9abebffc7582db62d9b96bb021ff9e.

--- src/hacks/Aimbot.cpp
+++ src/hacks/Aimbot.cpp
@@ -504,27 +504,19 @@ static void CreateMove()
     projectileAimbotRequired = false;
     bool should_zoom         = *auto_zoom;
     switch (weapon_mode)
-    switch(get_weapon_mode)
     {
-        case weapon_hitscan:
+    case weapon_hitscan:
+        if (should_backtrack)
+            UpdateShouldBacktrack();
+        target_last = RetrieveBestTarget(aimkey_status);
+        if (target_last)
         {
-            if(should_backtrack)
-                updateShouldBacktrack();
-            if(small_box_checker(target_entity))
-            {
-                    int weapon_case = LOCAL_W->m_iClassID();
-                    doAutoZoom(true, target_last);
-                    /* very much simple ""fix"" for the aimbot death stare */
-                    if (g_pLocalPlayer->holding_sniper_rifle && g_pLocalPlayer->bZoomed && CE_GOOD(LOCAL_W) && re::C_BaseCombatWeapon::GetSlot(RAW_ENT(LOCAL_W)) + 1 != 3)
-                        Aim(target_entity);
-                    else if (!g_pLocalPlayer->holding_sniper_rifle)
-                        Aim(target_entity);
-
-                    if(!hitscan_special_cases(target_entity, weapon_case))
-                        DoAutoshoot();
-                    else if (hitscan_special_cases(target_entity, weapon_case) && (CE_INT(LOCAL_W, netvar.m_iClip1) == 0))
-                        DoAutoshoot();
-            }
+            if (should_zoom)
+                DoAutoZoom(true, target_last);
+            int weapon_case = LOCAL_W->m_iClassID();
+            if (!HitscanSpecialCases(target_last, weapon_case))
+                DoAutoshoot(target_last);
+        }
         break;
     case weapon_melee:
         if (should_backtrack)

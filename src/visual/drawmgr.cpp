938e7114ddd93e8ea4365bf2a5e6dbf1d69cee6d
rosne <68449858+rosne-gamingyt@users.noreply.github.com>
2023-07-04 16:21:15 -0400

    wait a min

--- src/visual/drawmgr.cpp
+++ src/visual/drawmgr.cpp
@@ -6,7 +6,6 @@
  */
 
 #include <MiscTemporary.hpp>
-#include <hacks/Aimbot.hpp>
 #include <hacks/hacklist.hpp>
 #if ENABLE_IMGUI_DRAWING
 #include "imgui/imrenderer.hpp"
@@ -16,34 +15,27 @@
 #include <glez/draw.hpp>
 #endif
 #include <settings/Bool.hpp>
-#include <settings/Float.hpp>
-#include <settings/Rgba.hpp>
 #include <menu/GuiInterface.hpp>
 #include "common.hpp"
 #include "visual/drawing.hpp"
 #include "hack.hpp"
-#include "menu/menu/Menu.hpp"
 #include "drawmgr.hpp"
 
 static settings::Boolean info_text{ "hack-info.enable", "true" };
-static settings::Int info_style{ "hack-info.style", "0" };
-static settings::Rgba info_background_color{"hack-info.background", "000000b3"};
-static settings::Rgba info_foreground_color{"hack-info.foreground", "ffffff"};
-static settings::Int info_x{"hack-info.x", "10"};
-static settings::Int info_y{"hack-info.y", "10"};
+static settings::Boolean info_text_min{ "hack-info.minimal", "false" };
 
-void render_cheat_visuals()
+void RenderCheatVisuals()
 {
     {
-        PROF_SECTION(BeginCheatVisuals);
+        PROF_SECTION(BeginCheatVisuals)
         BeginCheatVisuals();
     }
     {
-        PROF_SECTION(DrawCheatVisuals);
+        PROF_SECTION(DrawCheatVisuals)
         DrawCheatVisuals();
     }
     {
-        PROF_SECTION(EndCheatVisuals);
+        PROF_SECTION(EndCheatVisuals)
         EndCheatVisuals();
     }
 }
@@ -65,60 +57,45 @@ void BeginCheatVisuals()
     ResetStrings();
 }
 
-
-double getRandom(double lower_bound, double upper_bound)
-{
-    std::uniform_real_distribution<double> unif(lower_bound, upper_bound);
-    static std::mt19937 rand_engine(std::time(nullptr));
-
-    double x = unif(rand_engine);
-    return x;
-}
-
 void DrawCheatVisuals()
 {
     {
-        PROF_SECTION(DRAW_info);
+        PROF_SECTION(DRAW_info)
         std::string name_s, reason_s;
-        PROF_SECTION(PT_info_text);
-        if (*info_text)
+        PROF_SECTION(PT_info_text)
+        if (*info_text && (!g_IEngine->IsConnected() || g_IEngine->Con_IsVisible()))
         {
-            float w, h;
-            std::string hack_info_text;
-            if (*info_style == 0) {
-                hack_info_text = "Rosnehook InDev " + hack::GetVersion() + 
-                "\nPress '" + open_gui_button.toString() + "' to open the menu.";
-                fonts::center_screen->stringSize(hack_info_text, &w, &h);
-                draw::Rectangle(*info_x - 5, *info_y - 5, w + 10, h + 10, *info_background_color);
-                draw::String(*info_x, *info_y, *info_foreground_color, hack_info_text.c_str(), *fonts::center_screen);
-            }
-            else if (*info_style == 1) {
-                hack_info_text = "Rosnehook InDev " + hack::GetVersion();
-                fonts::center_screen->stringSize(hack_info_text, &w, &h);
-                draw::Rectangle(*info_x - 5, *info_y - 5, w + 10, h + 10, *info_background_color);
-                draw::String(*info_x, *info_y, *info_foreground_color, hack_info_text.c_str(), *fonts::center_screen);
+            auto color = colors::RainbowCurrent();
+            color.a    = 1.0f;
+            AddSideString("rosnehook by rosneburgerworks", color);
+            if (!*info_text_min)
+            {
+                AddSideString(hack::GetVersion(), colors::gui); // GitHub commit and date
+                AddSideString(hack::GetType(), colors::gui);    // Compile type
+#if ENABLE_GUI
+                AddSideString("Press '" + open_gui_button.toString() + "' key to open/close cheat menu.", colors::gui);
+                AddSideString("Use mouse to navigate in menu.", colors::gui);
+#endif
             }
         }
     }
     if (spectator_target)
-    {
         AddCenterString("Press SPACE to stop spectating");
-    }
     {
-        PROF_SECTION(DRAW_WRAPPER);
+        PROF_SECTION(DRAW_WRAPPER)
         EC::run(EC::Draw);
     }
-    if (CE_GOOD(g_pLocalPlayer->entity) && !g_Settings.bInvalid)
+    if (CE_GOOD(LOCAL_E))
     {
         Prediction_PaintTraverse();
     }
     {
-        PROF_SECTION(DRAW_strings);
+        PROF_SECTION(DRAW_strings)
         DrawStrings();
     }
 #if ENABLE_GUI
     {
-        PROF_SECTION(DRAW_GUI);
+        PROF_SECTION(DRAW_GUI)
         gui::draw();
     }
 #endif

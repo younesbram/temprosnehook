716b34ad8882dd1fe8532ebc03dd6a8b46a84604
rosne <68449858+rosne-gamingyt@users.noreply.github.com>
2023-07-07 19:01:31 -0400

    Update MiscTemporary.cpp

--- src/MiscTemporary.cpp
+++ src/MiscTemporary.cpp
@@ -52,16 +52,18 @@ static InitRoutine misc_init(
     {
         static auto cl_sendmove_addr = CSignature::GetEngineSignature("55 89 E5 57 56 53 81 EC 2C 10 00 00 C6 85 ? ? ? ? 01");
         // Order matters!
-        cl_warp_sendmovedetour.Init(cl_sendmove_addr, (void *) hacks::warp::CL_SendMove_hook);
-        cl_nospread_sendmovedetour.Init(cl_sendmove_addr, (void *) hacks::nospread::CL_SendMove_hook);
+        cl_warp_sendmovedetour.Init(cl_sendmove_addr, (void*)hacks::warp::CL_SendMove_hook);
+        cl_nospread_sendmovedetour.Init(cl_sendmove_addr, (void*)hacks::nospread::CL_SendMove_hook);
 
         static std::optional<BytePatch> patch;
         static std::optional<BytePatch> patch2;
+
         print_r.installChangeCallback(color_callback);
         print_g.installChangeCallback(color_callback);
         print_b.installChangeCallback(color_callback);
+
         no_scope.installChangeCallback(
-            [](settings::VariableBase<bool> &, bool after)
+            [](settings::VariableBase<bool>&, bool after)
             {
                 if (!patch)
                 {
@@ -70,6 +72,7 @@ static InitRoutine misc_init(
                     // Keep rifle visible
                     patch2 = BytePatch(CSignature::GetClientSignature, "74 ? A1 ? ? ? ? 8B 40 ? 85 C0 75 ? C9", 0x0, { 0x70 });
                 }
+
                 if (after)
                 {
                     patch->Patch();
@@ -83,13 +86,17 @@ static InitRoutine misc_init(
                         patch2->Shutdown();
                 }
             });
+
         no_zoom.installChangeCallback(
-            [](settings::VariableBase<bool> &, bool after)
+            [](settings::VariableBase<bool>&, bool after)
             {
                 // std::optional so the addresses are searched when needed, not on inject
                 if (!patch2)
+                {
                     // Keep rifle visible
                     patch2 = BytePatch(CSignature::GetClientSignature, "74 ? A1 ? ? ? ? 8B 40 ? 85 C0 75 ? C9", 0x0, { 0x70 });
+                }
+
                 if (after)
                 {
                     if (no_scope)
@@ -100,8 +107,9 @@ static InitRoutine misc_init(
                     patch2->Shutdown();
                 }
             });
+
         nolerp.installChangeCallback(
-            [](settings::VariableBase<bool> &, bool after)
+            [](settings::VariableBase<bool>&, bool after)
             {
                 if (!after)
                 {
@@ -114,26 +122,31 @@ static InitRoutine misc_init(
                 else
                 {
                     backup_lerp = cl_interp->GetFloat();
+
                     // We should adjust cl_interp to be as low as possible
                     if (cl_interp->GetFloat() > 0.152f)
                         cl_interp->SetValue(0.152f);
                 }
             });
+
         EC::Register(
             EC::Shutdown,
             []()
             {
                 cl_warp_sendmovedetour.Shutdown();
                 cl_nospread_sendmovedetour.Shutdown();
+
                 if (backup_lerp)
                 {
                     cl_interp->SetValue(backup_lerp);
                     backup_lerp = 0.0f;
                 }
+
                 patch.reset();
                 patch2.reset();
             },
             "misctemp_shutdown");
+
 #if ENABLE_TEXTMODE
         // Ensure that we trigger the callback for textmode builds
         nolerp = false;

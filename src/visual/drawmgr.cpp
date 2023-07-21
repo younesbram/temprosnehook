559e3a4459aca87628c843cd88a34f1f00c033fc
cowhookdev <136142189+cowhookdev@users.noreply.github.com>
2023-07-21 19:39:00 +0000

    Update drawmgr.cpp

--- src/visual/drawmgr.cpp
+++ src/visual/drawmgr.cpp
@@ -128,7 +128,7 @@ void DrawCheatVisuals()
     if (CE_GOOD(g_pLocalPlayer->entity) && !g_Settings.bInvalid)
     {
         PROF_SECTION(DRAW_skinchanger);
-        hacks::skinchanger::DrawText();
+        //hacks::skinchanger::DrawText();
         Prediction_PaintTraverse();
     }
     {

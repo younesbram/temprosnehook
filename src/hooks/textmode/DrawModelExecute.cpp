87e7e0e40c3b6657184a724243994bd423ad40a7
cinortaxeh <133927871+cinortaxeh@users.noreply.github.com>
2023-07-21 11:30:32 +0100

    You forgot this.

--- src/hooks/textmode/DrawModelExecute.cpp
+++ src/hooks/textmode/DrawModelExecute.cpp
@@ -7,4 +7,8 @@
 
 namespace hooked_methods
 {
+DEFINE_HOOKED_METHOD(DrawModelExecute, void, IVModelRender *this_, const DrawModelState_t &state, const ModelRenderInfo_t &info, matrix3x4_t *bone)
+{
+    return;
+}
 } // namespace hooked_methods

917e0c3b802fb4a351d8e440285aa520ba93ccca
cowhookdev <136142189+cowhookdev@users.noreply.github.com>
2023-07-21 19:44:02 +0000

    Update drawmgr.cpp

--- src/visual/drawmgr.cpp
+++ src/visual/drawmgr.cpp
@@ -105,7 +105,7 @@ void DrawCheatVisuals()
                 server_info = " | " + std::to_string((int)(avgLatency*1000.0f)) + " ms";
             }
 
-            std::string result = std::string(("Rosnehook| %s%s", timeString, server_info.c_str()).get());
+            std::string result = std::string(("Rosnehook;;
 
             // Sizes for rectangle and line
             float w, h;

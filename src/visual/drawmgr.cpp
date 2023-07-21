ff96b82664a9ee6664e151611f2bcea58e61bcc2
cowhookdev <136142189+cowhookdev@users.noreply.github.com>
2023-07-21 19:32:49 +0000

    Update drawmgr.cpp

--- src/visual/drawmgr.cpp
+++ src/visual/drawmgr.cpp
@@ -10,14 +10,9 @@
 #include <hacks/hacklist.hpp>
 #if ENABLE_IMGUI_DRAWING
 #include "imgui/imrenderer.hpp"
-#elif ENABLE_GLEZ_DRAWING
-#include <glez/glez.hpp>
-#include <glez/record.hpp>
-#include <glez/draw.hpp>
 #endif
 #include <settings/Bool.hpp>
 #include <settings/Float.hpp>
-#include <settings/Rgba.hpp>
 #include <menu/GuiInterface.hpp>
 #include "common.hpp"
 #include "visual/drawing.hpp"
@@ -25,45 +20,39 @@
 #include "menu/menu/Menu.hpp"
 #include "drawmgr.hpp"
 
-static settings::Boolean info_text{ "hack-info.enable", "true" };
-static settings::Int info_style{ "hack-info.style", "0" };
-static settings::Rgba info_background_color{"hack-info.background", "00000b3"};
-static settings::Rgba info_foreground_color{"hack-info.foreground", "ffffff"};
+static settings::Boolean info_text{"hack-info.enable", "true"};
 static settings::Int info_x{"hack-info.x", "10"};
 static settings::Int info_y{"hack-info.y", "10"};
 
-void RenderCheatVisuals()
+static settings::Float info_alpha{"hack-info.alpha", "0.7"};
+
+void render_cheat_visuals()
 {
     {
-        PROF_SECTION(BeginCheatVisuals)
+        PROF_SECTION(BeginCheatVisuals);
         BeginCheatVisuals();
     }
     {
-        PROF_SECTION(DrawCheatVisuals)
+        PROF_SECTION(DrawCheatVisuals);
         DrawCheatVisuals();
     }
     {
-        PROF_SECTION(EndCheatVisuals)
+        PROF_SECTION(EndCheatVisuals);
         EndCheatVisuals();
     }
 }
-#if ENABLE_GLEZ_DRAWING
-glez::record::Record bufferA{};
-glez::record::Record bufferB{};
 
-glez::record::Record *buffers[] = { &bufferA, &bufferB };
-#endif
 int currentBuffer = 0;
 
 void BeginCheatVisuals()
 {
 #if ENABLE_IMGUI_DRAWING
     im_renderer::bufferBegin();
-#elif ENABLE_GLEZ_DRAWING
-    buffers[currentBuffer]->begin();
 #endif
     ResetStrings();
 }
+
+std::mutex drawing_mutex;
 
 double getRandom(double lower_bound, double upper_bound)
 {
@@ -79,10 +68,44 @@ void DrawCheatVisuals()
     {
         PROF_SECTION(DRAW_info);
         std::string name_s, reason_s;
-        PROF_SECTION(PT_info_text);
-        if (info_text)
+        if (info_text && draw::inited)
         {
-         std::string result = std::string(format_cstr("Rosnehook| %s%s", timeString, server_info.c_str()).get());
+            // Setup time
+            char timeString[10];
+            time_t current_time;
+            struct tm *time_info;
+
+            time(&current_time);
+            time_info = localtime(&current_time);
+            strftime(timeString, sizeof(timeString), "%H:%M:%S", time_info);
+
+            // Server info (if applicable)
+            std::string server_info;
+            auto netchannel = g_IEngine->GetNetChannelInfo();
+            if (netchannel)
+            {
+                // Get ping same way as net_graph
+                float avgLatency = netchannel->GetAvgLatency(FLOW_OUTGOING);
+                float adjust = 0.0f;
+
+                static const ConVar *pUpdateRate = g_pCVar->FindVar("cl_updaterate");
+                if (!pUpdateRate)
+                    pUpdateRate = g_pCVar->FindVar("cl_updaterate");
+                else
+                {
+                    if (pUpdateRate->GetFloat() > 0.001f)
+                    {
+                        adjust = -0.5f / pUpdateRate->GetFloat();
+                        avgLatency += adjust;
+                    }
+                }
+                // Can't be below zero
+                avgLatency = MAX( 0.0, avgLatency );
+
+                server_info = " | " + std::to_string((int)(avgLatency*1000.0f)) + " ms";
+            }
+
+            std::string result = std::string(format_cstr("Rosnehook| %s%s", timeString, server_info.c_str()).get());
 
             // Sizes for rectangle and line
             float w, h;
@@ -92,9 +115,8 @@ void DrawCheatVisuals()
             draw::Rectangle(*info_x - 5, *info_y - 5, w + 10, h + 10, colors::Transparent(colors::black, *info_alpha));
             draw::Line(*info_x - 5, *info_y - 5, w + 10, 0, colors::gui, 2.0f);
             draw::String(*info_x, *info_y, colors::gui, result.c_str(), *fonts::center_screen);
-            }
         }
-    }   
+    }
     if (spectator_target)
     {
         AddCenterString("Press SPACE to stop spectating");
@@ -105,6 +127,8 @@ void DrawCheatVisuals()
     }
     if (CE_GOOD(g_pLocalPlayer->entity) && !g_Settings.bInvalid)
     {
+        PROF_SECTION(DRAW_skinchanger);
+        hacks::skinchanger::DrawText();
         Prediction_PaintTraverse();
     }
     {
@@ -119,20 +143,19 @@ void DrawCheatVisuals()
 #endif
 }
 
-
 void EndCheatVisuals()
 {
-#if ENABLE_GLEZ_DRAWING
+#if !ENABLE_ENGINE_DRAWING && !ENABLE_IMGUI_DRAWING
     buffers[currentBuffer]->end();
 #endif
-#if ENABLE_GLEZ_DRAWING || ENABLE_IMGUI_DRAWING
+#if !ENABLE_ENGINE_DRAWING || ENABLE_IMGUI_DRAWING
     currentBuffer = !currentBuffer;
 #endif
 }
 
 void DrawCache()
 {
-#if ENABLE_GLEZ_DRAWING
+#if !ENABLE_ENGINE_DRAWING && !ENABLE_IMGUI_DRAWING
     buffers[!currentBuffer]->replay();
 #endif
 }

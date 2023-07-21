3851a337313a7093fa2fe7aac1e7d31799c6e8ee
rosne <68449858+rosne-gamingyt@users.noreply.github.com>
2023-07-21 16:02:29 -0400

    safdsafdafds

--- src/visual/drawmgr.cpp
+++ src/visual/drawmgr.cpp
@@ -86,7 +86,7 @@ void DrawCheatVisuals()
             std::string hack_info_text;
             if (*info_style == 0) {
                 hack_info_text = "Rosnehook InDev " + hack::GetVersion();
-                *fonts::center_screen->stringSize(hack_info_text, &w, &h);
+                fonts::center_screen->stringSize(hack_info_text, &w, &h);
                 draw::Rectangle(*info_x - 5.0f, *info_y - 5.0f, w + 10.0f, h + 10.0f, *info_background_color);
                 draw::String(*info_x, *info_y, *info_foreground_color, hack_info_text.c_str(), *fonts::center_screen);
             }

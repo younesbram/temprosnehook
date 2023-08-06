59e0b2567be1f3b3ded795f08a734e3710a70060
Runker45 <137824545+Runker45@users.noreply.github.com>
2023-08-05 19:48:33 -0700

    Update Aimbot.cpp

--- src/hacks/Aimbot.cpp
+++ src/hacks/Aimbot.cpp
@@ -1,11 +1,9 @@
-// this aimbot sucks ass asf
 /*
  * Aimbot.cpp
  *
  *  Created on: Oct 9, 2016
  *      Author: nullifiedcat
  */
-// To do: findout the DEFUALT FUCKING SETTINGS 
 #include <hacks/Aimbot.hpp>
 #include <hacks/AntiAim.hpp>
 #include <hacks/ESP.hpp>
@@ -53,13 +51,13 @@ static settings::Boolean auto_unzoom{ "aimbot.auto.unzoom", "true" };
 static settings::Float zoom_distance{ "aimbot.zoom.distance", "1250.0" };
 
 static settings::Boolean backtrack_aimbot{ "aimbot.backtrack", "false" };
-static settings::Boolean backtrack_last_tick_only("aimbot.backtrack.only-last-tick", "true");
+static settings::Boolean backtrack_last_tick_only("aimbot.backtrack.only-last-tick", "false");
 static bool force_backtrack_aimbot = false;
 // wtf is this above
 static settings::Float max_range{ "aimbot.target.max-range", "4096" };
-static settings::Boolean ignore_vaccinator{ "aimbot.target.ignore-vaccinator", "true" };
+static settings::Boolean ignore_vaccinator{ "aimbot.target.ignore-vaccinator", "false" };
 static settings::Boolean buildings_sentry{ "aimbot.target.sentry", "true" };
-static settings::Boolean npcs{ "aimbot.target.npcs", "true" };
+static settings::Boolean npcs{ "aimbot.target.npcs", "false" };
 static settings::Int teammates{ "aimbot.target.teammates", "0" };
 
 settings::Boolean engine_projpred{ "aimbot.debug.engine-pp", "true" }; // lags harder than my dad + crash
@@ -261,7 +259,6 @@ std::vector<Vector> GetValidHitpoints(CachedEntity *ent, int hitbox)
             ++i;
         }
     }
-
     return hitpoints;
 }
 
@@ -388,7 +385,7 @@ void DoAutoZoom(bool target_found, CachedEntity *target)
     // Keep track of our zoom time
     static Timer zoom_time{};
 
-    // Minigun spun up handler
+    // Minigun spun up handler + doesnt work properly. 
     if (*auto_spin_up && LOCAL_W->m_iClassID() == CL_CLASS(CTFMinigun))
     {
         if (target_found)

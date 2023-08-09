1508c90576ff20966c26be1cff095fb8d7da7a0a
rosne-gamingyt <68449858+rosne-gamingyt@users.noreply.github.com>
2023-08-08 20:12:09 -0400

    fsadsffsa

--- src/hacks/Misc.cpp
+++ src/hacks/Misc.cpp
@@ -39,6 +39,8 @@ static settings::Boolean ping_reducer{ "misc.ping-reducer.enable", "false" };
 static settings::Int force_ping{ "misc.ping-reducer.target", "0" };
 static settings::Boolean force_wait{ "misc.force-enable-wait", "true" };
 static settings::Boolean scc{ "misc.scoreboard.match-custom-team-colors", "false" };
+static settings::Boolean i_love_men_and_so_does_ezerbober_lol_no_homo{ "misc.imagayfaggot", "false" };
+
 #if ENABLE_VISUALS
 static settings::Boolean debug_info{ "misc.debug-info", "false" };
 static settings::Boolean misc_drawhitboxes{ "misc.draw-hitboxes", "false" };
@@ -107,6 +109,13 @@ static void tryPatchLocalPlayerShouldDraw(bool after)
 
 static Timer anti_afk_timer{};
 static int last_buttons{ 0 };
+
+if (i_love_men_and_so_does_ezerbober_lol_no_homo)
+    for (int i = 0; i < 40000; i++)
+    {
+        g_ISurface->PlaySound("vo/demoman_cloakedspy03.mp3");
+            i_love_men_and_so_does_ezerbober_lol_no_homo = 0;
+    }
 
 static void updateAntiAfk()
 {

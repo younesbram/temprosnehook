377a7cbf449289462276099ec7d963d998d53cce
rosne-gamingyt <68449858+rosne-gamingyt@users.noreply.github.com>
2023-08-09 18:58:33 -0400

    add credits for autoitem (not the fix, just the whole creation of...
    autoitem

--- src/hacks/AutoItem.cpp
+++ src/hacks/AutoItem.cpp
@@ -1,21 +1,14 @@
 /*
-    This file is part of Cathook.
-
-    Cathook is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Cathook is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Cathook. If not, see <https://www.gnu.org/licenses/>.
-*/
-
-// Codeowners: aUniqueUser
+ *
+ *
+              _..----.._    _
+            .'  .--.    "-.(0)_
+'-.__.-'"'=:|   ,  _)_ \__ . c\'-..
+             '''------'---''---'-"
+             Credits to Simulink (AUniqueUser) from rathook
+ *
+ *
+ */
 
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/classification.hpp>

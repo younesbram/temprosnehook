0bb898db4a2ea7571f0fae02a08695e8ae539bf9
Hexatronic <133927871+cinortaxeh@users.noreply.github.com>
2023-08-12 22:24:52 +0100

    e

--- src/chatstack.cpp
+++ src/chatstack.cpp
@@ -27,9 +27,6 @@ void OnCreateMove()
         if (!msg.text.empty())
         {
             // logging::Info("Saying %s %i", msg.text.c_str(), msg.text.size());
-            if (msg.team)
-                g_IEngine->ServerCmd(format("say_team \"", msg.text.c_str(), '"').c_str());
-            else
                 g_IEngine->ServerCmd(format("say \"", msg.text.c_str(), '"').c_str());
             last_say = g_GlobalVars->curtime;
         }

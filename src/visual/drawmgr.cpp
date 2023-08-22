e2a6ca640e06b369ae912a70418ed87640c898ec
Rosne <68449858+rosne-gamingyt@users.noreply.github.com>
2023-08-22 01:29:57 -0400

    anti crash

--- src/visual/drawmgr.cpp
+++ src/visual/drawmgr.cpp
@@ -21,11 +21,8 @@
 #include "hack.hpp"
 #include "drawmgr.hpp"
 
-static settings::Boolean info_text{"hack-info.enable", "true"};
-static settings::Int info_x{"hack-info.x", "10"};
-static settings::Int info_y{"hack-info.y", "10"};
-
-static settings::Float info_alpha{"hack-info.alpha", "0.7"};
+static settings::Boolean info_text{ "hack-info.enable", "true" };
+static settings::Boolean info_text_min{ "hack-info.minimal", "false" };
 
 void RenderCheatVisuals()
 {
@@ -67,50 +64,15 @@ void DrawCheatVisuals()
         std::string name_s, reason_s;
         if (*info_text && (!g_IEngine->IsConnected() || g_IEngine->Con_IsVisible()))
         {
-            // Setup time
-            char timeString[10];
-            time_t current_time;
-            struct tm *time_info;
-
-            time(&current_time);
-            time_info = localtime(&current_time);
-            strftime(timeString, sizeof(timeString), "%H:%M:%S", time_info);
-
-            // Server info (if applicable)
-            std::string server_info;
-            auto netchannel = g_IEngine->GetNetChannelInfo();
-            if (netchannel)
+            auto color = colors::RainbowCurrent();
+            color.a    = 1.0f;
+            AddSideString("Rosnehook | TF2", color);
+            if (!*info_text_min)
             {
-                // Get ping same way as net_graph
-                float avgLatency = netchannel->GetAvgLatency(FLOW_OUTGOING);
-                float adjust = 0.0f;
-
-                static const ConVar *pUpdateRate = g_pCVar->FindVar("cl_updaterate");
-                if (!pUpdateRate)
-                    pUpdateRate = g_pCVar->FindVar("cl_updaterate");
-                else
-                {
-                    if (pUpdateRate->GetFloat() > 0.001f)
-                    {
-                        adjust = -0.5f / pUpdateRate->GetFloat();
-                        avgLatency += adjust;
-                    }
-                }
-                // cannot be below zero
-                avgLatency = MAX( 0.0, avgLatency );
-
-                server_info = " | " + std::to_string((int)(avgLatency*1000.0f)) + " ms";
+#if ENABLE_GUI
+                AddSideString("Press '" + open_gui_button.toString() + "' key to open/close cheat menu.", colors::gui);
+#endif
             }
-
-            std::string result = std::string(strfmt("Rosnehook InDev | %s%s", timeString, server_info.c_str()).get());
-
-            // size for rectangle and line
-            float w, h;
-            fonts::center_screen->stringSize(result, &w, &h);
-
-            // draw
-            draw::Line(*info_x - 5, *info_y - 5, w + 10, 0, colors::gui, 2.0f);
-            draw::String(*info_x, *info_y, colors::gui, result.c_str(), *fonts::center_screen);
         }
     }
     if (spectator_target)

3dc4696744aea3feb11e5756711169f3b34e1d4d
Rosne <68449858+rosne-gamingyt@users.noreply.github.com>
2023-09-16 20:25:32 -0400

    revert axd gay commits
    This reverts commit 83a1ea43132c15d8fccfb8938b39cadc47647213.
    
    Revert "i hate nword"
    
    This reverts commit da9de30fd07effcc06c62e4eedbb768818a2b283.
    
    Revert "rosne told me to leave the code"
    
    This reverts commit 72ca44ee80c07dc36b662d36af6abbe1f752462f.
    
    Revert "rosne u forgot to do this in spam.cpp"
    
    This reverts commit 7659331e7eff22c1fbfcfdb88fb896fdb701c9e3.
    
    Revert "this some reason takes longer to crash the local player"
    
    This reverts commit 0414dcdeca1aa1e8cd07526679a4bb34a6ac0780.

--- src/visual/menu/menu/objects/container/Box.cpp
+++ src/visual/menu/menu/objects/container/Box.cpp
@@ -11,7 +11,7 @@
 #include <menu/Menu.hpp>
 namespace zerokernel_box
 {
-static settings::RVariable<rgba_t> color_border{ "zk.style.box.color.border", "353535ff" };
+static settings::RVariable<rgba_t> color_border{ "zk.style.box.color.border", "000000ff" };
 }
 void zerokernel::Box::render()
 {

091a2b31e100e1362c6d8a0e1404751c06c1095d
Rosne <68449858+rosne-gamingyt@users.noreply.github.com>
2023-10-22 16:35:30 -0400

    INSANE SUPER VAC UNDETECTION 2014

--- src/textmode.cpp
+++ src/textmode.cpp
@@ -1,25 +1,11 @@
-/*
- * textmode.cpp
- *
- *  Created on: Jul 28, 2017
- *      Author: nullifiedcat
- */
-
 #include "common.hpp"
 
 bool *allowSecureServers{ nullptr };
 
-// valve pls no ban
-
-void EXPOSED_Epic_VACBypass_1337_DoNotSteal_xXx_$1_xXx_MLG()
+void EXPOSED_Epic_VACBypass_1337_DoNotSteal_xXx_$1_xxx_MLG() 
 {
-    ((ICommandLine * (*) ()) dlsym(sharedobj::libtier0().lmap, "CommandLine_Tier0"))()->RemoveParm("-textmode");
-    ((ICommandLine * (*) ()) dlsym(sharedobj::libtier0().lmap, "CommandLine_Tier0"))()->RemoveParm("-insecure");
     uintptr_t Host_IsSecureServerAllowed_addr  = CSignature::GetEngineSignature("55 89 E5 83 EC ? E8 ? ? ? ? 8B 10 C7 44 24 ? ? ? ? ? 89 04 24 FF 52 ? 85 C0 74 ? C6 05");
     uintptr_t Host_IsSecureServerAllowed2_addr = CSignature::GetEngineSignature("55 89 E5 83 EC ? E8 ? ? ? ? 8B 10 C7 44 24 ? ? ? ? ? 89 04 24 FF 52 ? 85 C0 0F");
-    // +0x21 = allowSecureServers
-    // logging::Info("1337 VAC bypass: 0x%08x",
-    // Host_IsSecureServerAllowed_addr);
     static BytePatch HostSecureServer(Host_IsSecureServerAllowed_addr, { 0x55, 0x89, 0xE5, 0x83, 0xEC, 0x18, 0x31, 0xC0, 0x40, 0xC9, 0xC3 });
     static BytePatch HostSecureServer2(Host_IsSecureServerAllowed2_addr, { 0x31, 0xC0, 0x40, 0xC3 });
     HostSecureServer.Patch();
@@ -37,22 +23,21 @@ void EXPOSED_Epic_VACBypass_1337_DoNotSteal_xXx_$1_xXx_MLG()
     logging::Info("Allow Secure Servers: %d", *allowSecureServers);
 }
 
-CatCommand fixvac("fixvac", "Lemme in to secure servers", []() { EXPOSED_Epic_VACBypass_1337_DoNotSteal_xXx_$1_xXx_MLG(); });
+CatCommand fixvac("fixvac", "Lemme in to secure servers", []() { EXPOSED_Epic_VACBypass_1337_DoNotSteal_xXx_$1_xxx_MLG(); });
 
-static InitRoutine init_textmode(
-    []()
-    {
+static InitRoutine init([]() 
+{
 #if ENABLE_TEXTMODE_STDIN
-        logging::Info("[TEXTMODE] Setting up input handling");
-        int flags = fcntl(0, F_GETFL, 0);
-        flags |= O_NONBLOCK;
-        fcntl(0, F_SETFL, flags);
-        logging::Info("[TEXTMODE] stdin is now non-blocking");
+    logging::Info("[TEXTMODE] Setting up input handling");
+    int flags = fcntl(0, F_GETFL, 0);
+    flags |= O_NONBLOCK;
+    fcntl(0, F_SETFL, flags);
+    logging::Info("[TEXTMODE] stdin is now non-blocking");
 #endif
 #if ENABLE_VAC_BYPASS
-        EXPOSED_Epic_VACBypass_1337_DoNotSteal_xXx_$1_xXx_MLG();
+    EXPOSED_Epic_VACBypass_1337_DoNotSteal_xXx_$1_xxx_MLG();
 #endif
-    });
+});
 
 #if ENABLE_TEXTMODE_STDIN
 void UpdateInput()
